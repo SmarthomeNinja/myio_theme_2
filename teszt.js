d3b3a29916fe37df26343d8cf60b00a49bfe09f0
Mac Pro <macpro@Peters-Mac-mini.local>
2026-02-04 10:47:29 +0100

    Auto-sync: 2026. 02. 04. 10:47:29

--- teszt.js
+++
@@ -1,9 +0,0 @@
-// Teszt fájl - létrehozva API-n keresztül
-console.log('Hello from teszt.js!');
-
-// Egyszerű függvény
-function tesztFunction() {
-    return 'Ez egy teszt függvény!';
-}
-
-module.exports = tesztFunction;
