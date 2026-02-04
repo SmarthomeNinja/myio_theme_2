e7917171b5e7e0b2dea62622a802a4f52bc3f13e
SmarthomeNinja <info@smarthome.ninja>
2026-02-04 10:46:42 +0100

    Add teszt.js file via API

---
+++ teszt.js
@@ -0,0 +1,9 @@
+// Teszt fájl - létrehozva API-n keresztül
+console.log('Hello from teszt.js!');
+
+// Egyszerű függvény
+function tesztFunction() {
+    return 'Ez egy teszt függvény!';
+}
+
+module.exports = tesztFunction;
