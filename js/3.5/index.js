0f6626450ed0df0a74b0b139289a276a11e0e3de
Mac Pro <macpro@Peters-Mac-mini.local>
2026-02-05 16:03:51 +0100

    Auto-sync: 2026. 02. 05. 16:03:51

--- js/3.5/index.js
+++ js/3.5/index.js
@@ -1251,7 +1251,7 @@ let isDraggingCard = false;
         font-size: 14px;
       }
       .myio-thermo-hyst {
-        font-size: minmax(11px, calc(11px * var(--myio-zoom)));
+        font-size: clamp(12px, 11px * var(--myio-zoom), 14px);
         color: #888;
         margin-top: 2px;
       }
