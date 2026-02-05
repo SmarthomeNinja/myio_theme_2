35c67ad6d49624f10b2a83ffa520242804bb1d10
Mac Pro <macpro@Peters-Mac-mini.local>
2026-02-05 13:28:17 +0100

    Auto-sync: 2026. 02. 05. 13:28:17

--- js/3.5/liveUpdate.js
+++ js/3.5/liveUpdate.js
@@ -275,7 +275,7 @@ const MyIOLive = (function() {
       thermoCards.forEach(card => {
         // Osztály frissítése
         card.classList.remove('myio-on', 'myio-off');
-        card.classList.add(isOn ? 'myio-cool' : 'myio-off');
+        card.classList.add(isOn ? 'myio-on' : 'myio-off');
         
         // Toggle input frissítése
         const toggle = card.querySelector('.myio-miniToggle input[type="checkbox"]');
