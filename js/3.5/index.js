e1d728295d3347c0630ad74d86337b501b02d65b
Mac Pro <macpro@Peters-Mac-mini.local>
2026-02-05 16:11:46 +0100

    Auto-sync: 2026. 02. 05. 16:11:46

--- js/3.5/index.js
+++ js/3.5/index.js
@@ -1221,7 +1221,7 @@ let isDraggingCard = false;
         pointer-events: none;
       }
       .myio-thermo-mode {
-        font-size: calc(20px * var(--myio-zoom));
+        font-size: clamp(0,calc(20px * var(--myio-zoom),20px));
         font-weight: 500;
         text-transform: uppercase;
         letter-spacing: 0.5px;
