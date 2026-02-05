524a8382612a9b8467b184d8cc70bec3b4e46605
Mac Pro <macpro@Peters-Mac-mini.local>
2026-02-05 22:02:19 +0100

    Auto-sync: 2026. 02. 05. 22:02:19

--- js/3.5/renderers.js
+++ js/3.5/renderers.js
@@ -63,7 +63,7 @@
   
       // ---- base line + emphasized chevron (UP) ----
       const base = document.createElementNS(SVG_NS, "path");
-      base.setAttribute("d", "M2 20H7L12 14L15.7 20H22");
+      base.setAttribute("d", "M2 20H7L12 14L17 20H22");
       base.setAttribute("fill", "none");
       svg.appendChild(base);  
       
