3ccc5f26734179566d47cf1ee1419654ad379b44
Mac Pro <macpro@Peters-Mac-mini.local>
2026-02-05 11:36:25 +0100

    Auto-sync: 2026. 02. 05. 11:36:25

--- js/3.5/index.js
+++ js/3.5/index.js
@@ -1149,12 +1149,12 @@ let isDraggingCard = false;
           try {
             const onInput = document.createElement("input");
             onInput.name = onName;
-            onInput.value = String(Math.round(currentOnVal * 10));
+            onInput.value = String(Math.round(currentOnVal ));
             changed(onInput, onInput.name, 10);
             
             const offInput = document.createElement("input");
             offInput.name = offName;
-            offInput.value = String(Math.round(currentOffVal * 10));
+            offInput.value = String(Math.round(currentOffVal));
             changed(offInput, offInput.name, 10);
           } catch (err) {
             console.error("Thermostat update error:", err);
