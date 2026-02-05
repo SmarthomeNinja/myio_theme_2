562b79e8ff1e694b742e7c58394b472ace7a5440
Mac Pro <macpro@Peters-Mac-mini.local>
2026-02-05 21:54:09 +0100

    Auto-sync: 2026. 02. 05. 21:54:09

--- js/3.5/renderers.js
+++ js/3.5/renderers.js
@@ -69,7 +69,7 @@
   
       const chevron = document.createElementNS(SVG_NS, "path");
       // A teljes szélességtartomány jobb kihasználása
-      chevron.setAttribute("d", "M4 16L12 8L20 16");  // 4-20 = 16 egység széles, 8 egység magas
+      chevron.setAttribute("d", "M4 12L10 8L22 12");  // 4-20 = 16 egység széles, 8 egység magas
       chevron.setAttribute("fill", "none");
       chevron.setAttribute("stroke-width", "1");
       chevron.setAttribute("stroke-linecap", "round");
