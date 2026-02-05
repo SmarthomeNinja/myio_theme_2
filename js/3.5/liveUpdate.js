8bbc64f841eea3564c6d238f98d65aa2ece9f1ca
Mac Pro <macpro@Peters-Mac-mini.local>
2026-02-05 13:42:07 +0100

    Auto-sync: 2026. 02. 05. 13:42:07

--- js/3.5/liveUpdate.js
+++ js/3.5/liveUpdate.js
@@ -290,7 +290,7 @@ const MyIOLive = (function() {
     }
     if(isThermoCard) {
       console.log('Thermo card detected in relays, ensure thermo-specific UI elements are updated');
-      console.log(PCA_min_temp_ON[9]);
+      console.log(PCA_min_temp_ON[6]);
       if (typeof window.renderThermo === 'function') {
         requestAnimationFrame(() => {
           try {
