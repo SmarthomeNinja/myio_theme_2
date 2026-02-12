d8f85076a7b3a3e7dd64ca32c977b32e836f2f46
Mac Pro <macpro@Peters-Mac-mini.local>
2026-02-12 20:00:01 +0100

    Auto-sync: 2026. 02. 12. 20:00:01

--- js/2.5/nav.js
+++ js/2.5/nav.js
@@ -865,14 +865,7 @@ function buildHeader() {
 	
 	footer.appendChild(btnLogout);
 
-	const btnInfo = document.createElement("button");
-	btnInfo.type = "button";
-	btnInfo.margin = "3px 8px";
-	btnInfo.className = "myio-btn small";
-	btnInfo.textContent = ("?");
-	btnInfo.onclick = (e) => { e.preventDefault(); window.location.href = "https://smarthomeninja.hu/dashboard/"; };
-
-	footer.appendChild(btnInfo);
+	
 	// Export / Import
 	const dataRow = document.createElement("div");
 	dataRow.className = "myio-menuRow";
