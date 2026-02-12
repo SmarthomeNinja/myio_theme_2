9eb7b18bd976023db3d056ecd3187d633529ab64
Mac Pro <macpro@Peters-Mac-mini.local>
2026-02-12 20:06:41 +0100

    Auto-sync: 2026. 02. 12. 20:06:41

--- js/2.5/nav.js
+++ js/2.5/nav.js
@@ -976,7 +976,7 @@ function buildHeader() {
 	btnInfo.className = "myio-iconBtn myio-settingsBtn";
 	btnSettings.setAttribute("aria-label", btnSettings.title);
 	btnInfo.textContent = ("i" /* info ikon */);
-	btnInfo.fontSize = "1.0em";
+	btnInfo.fontSize = "10px";
 	btnInfo.onclick = (e) => { e.preventDefault(); window.location.href = "https://smarthomeninja.hu/dashboard/"; };
 
 	right.appendChild(btnInfo);
